0118cf2db1da68537a219fdb8f389c48aa1d88ec
Pengfei Li <drazzi.lee@gmail.com>
2013-09-26 18:04:33 +0800

    修改了数据库结构。

--- toadwords.sql
+++ toadwords.sql
@@ -10,15 +10,40 @@ Target Server Type    : MYSQL
 Target Server Version : 50612
 File Encoding         : 65001
 
-Date: 2013-09-18 18:36:11
+Date: 2013-09-26 18:03:56
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
--- Table structure for `think_ad`
+-- Table structure for `adgroup`
 -- ----------------------------
-DROP TABLE IF EXISTS `think_ad`;
-CREATE TABLE `think_ad` (
+DROP TABLE IF EXISTS `adgroup`;
+CREATE TABLE `adgroup` (
+  `idclick_groupid` int(10) NOT NULL,
+  `idclick_uid` int(10) NOT NULL,
+  `idclick_planid` int(10) NOT NULL,
+  `adgroup_name` varchar(128) NOT NULL,
+  `adgroup_id` bigint(10) DEFAULT '0',
+  `campaign_id` bigint(10) DEFAULT NULL,
+  `cutomer_id` bigint(10) DEFAULT NULL,
+  `keywords` varchar(200) NOT NULL,
+  `budget_amount` decimal(10,2) NOT NULL COMMENT '广告组出价，以天为单位，adwords需要大于等于1',
+  `last_action` enum('CREATE','UPDATE','DELETE') NOT NULL,
+  `sync_status` enum('QUEUE','SYNCED','ERROR','RECEIVE') NOT NULL DEFAULT 'RECEIVE' COMMENT '在队列中，已同步，同步出错',
+  `adgroup_status` enum('ACTIVE','PAUSE','DELETE') NOT NULL DEFAULT 'ACTIVE',
+  PRIMARY KEY (`idclick_groupid`,`idclick_uid`,`idclick_planid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of adgroup
+-- ----------------------------
+INSERT INTO adgroup VALUES ('123456', '441', '12345', 'group_name2', '0', null, null, 'keywords3,keywords2,keywords1', '201.00', 'DELETE', 'RECEIVE', 'DELETE');
+
+-- ----------------------------
+-- Table structure for `adgroupad`
+-- ----------------------------
+DROP TABLE IF EXISTS `adgroupad`;
+CREATE TABLE `adgroupad` (
   `idclick_adid` int(10) NOT NULL,
   `idclick_uid` int(10) NOT NULL,
   `idclick_groupid` int(10) NOT NULL,
@@ -37,40 +62,15 @@ CREATE TABLE `think_ad` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of think_ad
+-- Records of adgroupad
 -- ----------------------------
-INSERT INTO think_ad VALUES ('12345', '441', '123456', '0', null, null, 'headline——l', 'description1....', 'description2....', 'http://www.izptec.com/go1.php', 'http://www.izptec.com/', 'DELETE', 'RECEIVE', 'ACTIVE');
+INSERT INTO adgroupad VALUES ('12345', '441', '123456', '0', null, null, 'headline——l', 'description1....', 'description2....', 'http://www.izptec.com/go1.php', 'http://www.izptec.com/', 'DELETE', 'RECEIVE', 'ACTIVE');
 
 -- ----------------------------
--- Table structure for `think_adgroup`
+-- Table structure for `campaign`
 -- ----------------------------
-DROP TABLE IF EXISTS `think_adgroup`;
-CREATE TABLE `think_adgroup` (
-  `idclick_groupid` int(10) NOT NULL,
-  `idclick_uid` int(10) NOT NULL,
-  `idclick_planid` int(10) NOT NULL,
-  `adgroup_name` varchar(128) NOT NULL,
-  `adgroup_id` bigint(10) DEFAULT '0',
-  `campaign_id` bigint(10) DEFAULT NULL,
-  `cutomer_id` bigint(10) DEFAULT NULL,
-  `keywords` varchar(200) NOT NULL,
-  `budget_amount` decimal(10,2) NOT NULL COMMENT '广告组出价，以天为单位，adwords需要大于等于1',
-  `last_action` enum('CREATE','UPDATE','DELETE') NOT NULL,
-  `sync_status` enum('QUEUE','SYNCED','ERROR','RECEIVE') NOT NULL DEFAULT 'RECEIVE' COMMENT '在队列中，已同步，同步出错',
-  `adgroup_status` enum('ACTIVE','PAUSE','DELETE') NOT NULL DEFAULT 'ACTIVE',
-  PRIMARY KEY (`idclick_groupid`,`idclick_uid`,`idclick_planid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of think_adgroup
--- ----------------------------
-INSERT INTO think_adgroup VALUES ('123456', '441', '12345', 'group_name2', '0', null, null, 'keywords3,keywords2,keywords1', '201.00', 'DELETE', 'RECEIVE', 'DELETE');
-
--- ----------------------------
--- Table structure for `think_campaign`
--- ----------------------------
-DROP TABLE IF EXISTS `think_campaign`;
-CREATE TABLE `think_campaign` (
+DROP TABLE IF EXISTS `campaign`;
+CREATE TABLE `campaign` (
   `idclick_planid` int(10) NOT NULL,
   `idclick_uid` int(10) NOT NULL,
   `campaign_id` bigint(10) DEFAULT '0' COMMENT 'Google广告系列ID',
@@ -89,15 +89,15 @@ CREATE TABLE `think_campaign` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of think_campaign
+-- Records of campaign
 -- ----------------------------
-INSERT INTO think_campaign VALUES ('12345', '441', '0', '5572928024', 'campaign_name #523976704166f', '10031,10032', '10031,10032', '1', '20000.00', 'ACCELERATED', '10.00', 'ACTIVE', 'CREATE', 'QUEUE');
+INSERT INTO campaign VALUES ('12345', '441', '0', '5572928024', 'campaign_name #523976704166f', '10031,10032', '10031,10032', '1', '20000.00', 'ACCELERATED', '10.00', 'ACTIVE', 'CREATE', 'QUEUE');
 
 -- ----------------------------
--- Table structure for `think_user`
+-- Table structure for `customer`
 -- ----------------------------
-DROP TABLE IF EXISTS `think_user`;
-CREATE TABLE `think_user` (
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
   `idclick_uid` int(10) NOT NULL,
   `adwords_customerid` bigint(10) DEFAULT NULL,
   `last_action` enum('CREATE','UPDATE','DELETE') NOT NULL,
@@ -108,6 +108,7 @@ CREATE TABLE `think_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of think_user
+-- Records of customer
 -- ----------------------------
-INSERT INTO think_user VALUES ('441', '5572928024', 'CREATE', 'RECEIVE', '0', '0');
+INSERT INTO customer VALUES ('441', '5572928024', 'CREATE', 'RECEIVE', '0', '0');
+INSERT INTO customer VALUES ('442', '1235154143', 'CREATE', 'RECEIVE', '0', '0');
