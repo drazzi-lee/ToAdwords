bcb35a3d1a88025bc5742e1ef5b56afcd13edfd3
Pengfei Li <drazzi.lee@gmail.com>
2013-10-08 13:06:01 +0800

    调整消息操作模块调度方法。

--- src/ToAdwords/MessageHandler.class.php
+++ src/ToAdwords/MessageHandler.class.php
@@ -7,8 +7,7 @@ use ToAdwords\Object\Adwords\AdGroup;
 use ToAdwords\Object\Adwords\AdGroupAd;
 use ToAdwords\Util\Log;
 use ToAdwords\Util\Message;
-
-use \Exception;
+use ToAdwords\Exceptions\MessageException;
 
 /**
  * 消息处理类
@@ -18,8 +17,48 @@ class MessageHandler{
 	
 
 	public function handle(Message $message){
+		$module = null;
+		$result = null;
 	
-	
+		switch($message->module){
+			case 'Customer':
+				$module = new Customer();	
+				break;
+			case 'Campaign':
+				$module = new Campaign();
+				break;
+			case 'AdGroup':
+				$module = new AdGroup();
+				break;
+			case 'AdGroupAd':
+				$module = new AdGroupAd();
+				break;
+			default:
+				throw new MessageException('解析错误，不能识别的module::'.$message['module']
+						.' 消息位置：'.$pos);
+		}
+		
+		switch($message->action){
+			case 'CREATE':
+				$result = $module->create($message['data']);
+				break;
+			case 'UPDATE':
+				$result = $module->update($message['data']);
+				break;
+			case 'DELETE':
+				$result = $module->delete($message['data']);
+				break;
+			default:
+				throw new MessageException('解析错误，不能识别的action::'.$message['action']
+						.' 消息位置：'.$pos);
+		}
+		
+		if(!$result){
+			$message_retry = $message;
+			$message_retry['error_count'] = 1;
+			$httpsqs->put(HTTPSQS_QUEUE_RETRY, json_encode($message_retry));			
+			throw new MessageException('发送消息失败，消息位置：'.$pos.' 消息内容：'.$data);
+		}
 	}
 	
 	public function call(){
--- test/daemon_common.php
+++ test/daemon_common.php
@@ -6,6 +6,9 @@ use ToAdwords\Util\Log;
 use ToAdwords\Util\Httpsqs;
 use ToAdwords\Util\Message;
 use ToAdwords\MessageHandler;
+use ToAdwords\Exceptions\MessageException;
+
+use \Exception;
 
 $httpsqs = new Httpsqs(HTTPSQS_HOST, HTTPSQS_PORT, HTTPSQS_AUTH);
 $queue_common = HTTPSQS_QUEUE_COMMON;
@@ -15,56 +18,19 @@ while(true){
 	$pos = $result['pos'];
 	$data = $result['data'];
 	if($data != 'HTTPSQS_GET_END' && $data != 'HTTPSQS_ERROR'){
-		$data_decode = json_decode($data);		
-		$message = new Message($data_decode['module'], $data_decode['action'], $data_decode['data']);
-		
-		$messageHandler = new MessageHandler();
-		
-		$messageHandler->handle($message);
-		
-		/* $module = null;
-		switch($message['module']){
-			case 'Customer':
-				$module = new Customer();	
-				break;
-			case 'Campaign':
-				$module = new Campaign();
-				break;
-			case 'AdGroup':
-				$module = new AdGroup();
-				break;
-			case 'AdGroupAd':
-				$module = new AdGroupAd();
-				break;
-			default:
-				throw new Exception('不能识别的module::'.$message['module'].' 消息位置：'.$pos);
+		try{
+			$data_decode = json_decode($data);	
+			$message = new Message($data_decode['module'], $data_decode['action'], $data_decode['data']);		
+			$messageHandler = new MessageHandler();		
+			$messageHandler->handle($message);
+			unset($data_decode, $message, $messageHandler);
+		} catch(MessageException $e) {
+			Log::write($e->getMessage(), __METHOD__, './daemon_common.log');
+		} catch(Exception $e){
+			Log::write('捕捉到未定义异常：：'.$e->getMessage());
 		}
-
-		switch($message['action']){
-			case 'CREATE':
-				$result = $module->create($message['data']);
-				break;
-			case 'UPDATE':
-				$result = $module->update($message['data']);
-				break;
-			case 'DELETE':
-				$result = $module->delete($message['data']);
-				break;
-			default:
-				throw new Exception('不能识别的action::'.$message['action'].' 消息位置：'.$pos);
-		}
-
-		//如果消息发送失败，则进入重试队列
-		if(!$result){
-			$message_retry = $message;
-			$message_retry['error_count'] = 1;
-			$httpsqs->put(HTTPSQS_QUEUE_RETRY, json_encode($message_retry));
-			Log::write('发送消息失败，消息位置：'.$pos.' 消息内容：'
-				.$data, __METHOD__, './daemon_common.log');
-		} */
-
 	} else {
+		unset($result, $pos, $data);
 		sleep(1);
 	}
 }
-
