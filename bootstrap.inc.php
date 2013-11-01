ee4382f4df095655bcbe5543369f3a67824116e8
Pengfei Li <drazzi.lee@gmail.com>
2013-11-01 14:00:56 +0800

    Update directory.

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -28,7 +28,7 @@ define('TOADWORDS_LOG_RECORD', TRUE);
 //数据库配置
 define('TOADWORDS_DSN', 'mysql:dbname=toadwords;host=127.0.0.1;charset=utf8');
 define('TOADWORDS_USER', 'root');
-define('TOADWORDS_PASS', 'qjklw');
+define('TOADWORDS_PASS', '123456');
 
 //消息队列配置
 define('HTTPSQS_HOST', '10.0.2.19');
